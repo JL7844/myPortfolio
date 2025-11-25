1e4271dca02697d4351644bf5aeaee90767d968b
Jl7844 <jossylegesse17@gmail.com>
2025-11-25 02:19:30 -0900

    fix githib pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' // remove if not using React
 
 export default defineConfig({
-  base: '/repo-name/',  // <-- Replace with your repo name exactly
+  base: '/myPortfolio/',  // <-- Replace with your repo name exactly
   plugins: [react()]
 })
