26d06da7a7132205b47c6ae52f5e1d4df011f04a
Jl7844 <jossylegesse17@gmail.com>
2025-11-25 02:09:28 -0900

    diploy vit eproject to github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' // remove if not using React
 
 export default defineConfig({
-  base: '/repo-name/',  // replace repo-name with your GitHub repo
+  base: '/repo-name/',  // <-- Replace with your repo name exactly
   plugins: [react()]
 })
